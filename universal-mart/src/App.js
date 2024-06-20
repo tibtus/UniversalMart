f1c45e949a0c2a4d64078da05cb4062dfd45e7c0
Serhii Burdeinyi <tibtus@gmail.com>
2024-06-20 14:44:53 +0300

    #new_project_all

---
+++ universal-mart/src/App.js
@@ -0,0 +1,19 @@
+import './styles.css';
+import Header from "./layout/Header";
+import Main from "./layout/Main";
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <Main/>
+      test
+
+    </div>
+  );
+}
+
+export default App;
